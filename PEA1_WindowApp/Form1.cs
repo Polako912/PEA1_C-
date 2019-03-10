a37a0956286978917fc6f2dc4950d886c0779089
Michał Polak <michal.polak37@gmail.com>
2019-03-10 18:07:44 +0100

    lekkie zmiany w głównym oknie

--- PEA1_WindowApp/Form1.cs
+++ PEA1_WindowApp/Form1.cs
@@ -12,16 +12,35 @@ namespace PEA1_WindowApp
 {
     public partial class Form1 : Form
     {
+        MainMenu mainMenu = new MainMenu();
+        MenuItem menuItem1 = new MenuItem("Plik");
+        MenuItem menuItem2 = new MenuItem("O programie");
+        MenuItem menuItem3 = new MenuItem("Zamknij");
+        MenuItem menuItem4 = new MenuItem("Wybierz plik");
+        MenuItem menuItem5 = new MenuItem("Opis");
+
         Button chooseFile = new Button();
         Button bfButton = new Button();
         Button BnBbutton = new Button();
         public Form1()
         {
+            mainMenu.MenuItems.Add(menuItem1);
+            menuItem1.MenuItems.Add(menuItem4);
+            mainMenu.MenuItems.Add(menuItem2);
+            menuItem2.MenuItems.Add(menuItem5);
+            mainMenu.MenuItems.Add(menuItem3);
+
             this.Controls.Add(chooseFile);
             this.Controls.Add(bfButton);
             this.Controls.Add(BnBbutton);
-            bfButton.Top = 30;
-            BnBbutton.Top = 60;
+            this.Menu = mainMenu;
+            chooseFile.Width = 120;
+            chooseFile.Top = 60;
+            chooseFile.Left = 350;
+            bfButton.Top = 90;
+            bfButton.Left = 350;
+            BnBbutton.Top = 120;
+            BnBbutton.Left = 350;
             bfButton.Width = 120;
             BnBbutton.Width = 120;
             chooseFile.Text = "Wybierz plik";
