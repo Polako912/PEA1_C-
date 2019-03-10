eea94f3caf17a343657c8a797c1c93a98627e589
Michał Polak <michal.polak37@gmail.com>
2019-03-10 17:27:37 +0100

    Stworzenie pierwszego okna z przyciskami

---
+++ PEA1_WindowApp/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PEA1_WindowApp
+{
+    public partial class Form1 : Form
+    {
+        Button chooseFile = new Button();
+        Button bfButton = new Button();
+        Button BnBbutton = new Button();
+        public Form1()
+        {
+            this.Controls.Add(chooseFile);
+            this.Controls.Add(bfButton);
+            this.Controls.Add(BnBbutton);
+            bfButton.Top = 30;
+            BnBbutton.Top = 60;
+            bfButton.Width = 120;
+            BnBbutton.Width = 120;
+            chooseFile.Text = "Wybierz plik";
+            bfButton.Text = "Algorytm BruteForce";
+            BnBbutton.Text = "Algorytm Branch & Bound";
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
